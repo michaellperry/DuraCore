d32b0510fdc096a2187711491ece52266939dbc8
Michael L Perry <michael@qedcode.com>
2013-01-20 14:50:41 -0600

    Created DuraCore project.

---
+++ DuraCore/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DuraCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "What can we get for you?";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Demo app for \"Don't Click Submit Twice!!!\"";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Michael L Perry";
+
+            return View();
+        }
+    }
+}

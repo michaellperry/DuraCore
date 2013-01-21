0ffaea2dc9ac56db19f7a7bb7124232d738a5d84
Michael L Perry <michael@qedcode.com>
2013-01-20 22:57:20 -0600

    Add to cart, checkout, and confirmation.

--- DuraCore/Controllers/HomeController.cs
+++ DuraCore/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DuraCore.Models;
 
 namespace DuraCore.Controllers
 {
@@ -12,7 +13,31 @@ namespace DuraCore.Controllers
         {
             ViewBag.Message = "What can we get for you?";
 
-            return View();
+            return View(new Line());
+        }
+
+        [HttpPost]
+        public ActionResult Checkout(Line line)
+        {
+            ViewBag.Message = "Here's what you ordered.";
+
+            return View(new ShoppingCart
+            {
+                Item = line.Item,
+                Uniquifier = Guid.NewGuid()
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Confirmation(ShoppingCart shoppingCart)
+        {
+            ViewBag.Message = "Thank you for your order.";
+
+            return View(new Confirmation
+            {
+                Item = shoppingCart.Item,
+                ConfirmationNumber = "123"
+            });
         }
 
         public ActionResult About()
---
+++ DuraCore/Models/Confirmation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DuraCore.Models
+{
+    public class Confirmation
+    {
+        public string Item { get; set; }
+        public string ConfirmationNumber { get; set; }
+    }
+}
